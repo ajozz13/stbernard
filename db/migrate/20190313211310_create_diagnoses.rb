4ff9dc63962fa48f7d26c5cf469deaafb784b747
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2019-03-15 06:57:00 -0400

    correct name

--- db/migrate/20190313211310_create_diagnoses.rb
+++ db/migrate/20190313211310_create_diagnoses.rb
@@ -2,7 +2,7 @@ class CreateDiagnoses < ActiveRecord::Migration[5.2]
   def change
     create_table :diagnoses do |t|
       t.integer :admission_id
-      t.string :coding_systen
+      t.string :coding_system
       t.string :code
       t.text :description
 
