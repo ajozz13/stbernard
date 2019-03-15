fbccb7694c78028e60d7c05b35c466533f4289ba
Alberto Ochoa <ajozz13@gmail.com>
2019-03-14 23:39:28 -0400

    use plural

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,6 +1,6 @@
 class Admission < ApplicationRecord
   belongs_to :patient
-  has_many :diagnosis
-  has_many :symptom
-  has_many :observation
+  has_many :diagnoses
+  has_many :symptoms
+  has_many :observations
 end
