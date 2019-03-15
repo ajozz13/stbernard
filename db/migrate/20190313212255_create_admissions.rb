bd5b2e6f7244b7fb538a900c4622ecc948886bb6
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2019-03-15 06:58:02 -0400

    setup table to use the appropriate id

--- db/migrate/20190313212255_create_admissions.rb
+++ db/migrate/20190313212255_create_admissions.rb
@@ -2,12 +2,10 @@ class CreateAdmissions < ActiveRecord::Migration[5.2]
   def change
     create_table :admissions do |t|
       t.datetime :moment
-      t.ingeger :patient_id
-=begin
+      t.integer :patient_id
       t.integer :diagnosis_id
       t.integer :symptom_id
       t.integer :observation_id
-=end
       t.timestamps
     end
   end
