8e8895efac6b8d1bd55b455b42ce8aea296f530b
Alberto Ochoa <ajozz13@gmail.com>
2019-03-14 23:29:08 -0400

    possible test for post with json input

--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -37,7 +37,10 @@ describe "Patient API POST Requests", :type => :request do
   # This post test works in postman but not here....
   it "Successful POST to: #{ url }" do
     hdrs = { "CONTENT_TYPE" => "application/json" }
-    post url, params: '{ "patient": { "first_name": "Mark", "middle_name": "M", "last_name": "Martinez", "honorific": "Mr", "dob": "1971-01-12", "gender": 0, "facility_id": 1 } }', headers: hdrs
+    params = { first_name: "Mark", middle_name: "M", last_name: "Martinez", honorific: "Mr", dob: "1971-01-12", gender: 0, facility_id: 1 }
+    post url, body: params.to_json, headers: hdrs
+    #post url, body: '{ "patient": { "first_name": "Mark", "middle_name": "M", "last_name": "Martinez", "honorific": "Mr", "dob": "1971-01-12", "gender": 0, "facility_id": 1 } }',
+      #format: 'json', headers: hdrs
     puts "#{ response.body }"
     #expect( response ).to be_successful
     expect( response.code ).to eq( "201" )
