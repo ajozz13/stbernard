93424a99a45ffb611f59c6662ad57e7ca7ee0657
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2019-03-15 07:11:16 -0400

    Admission links to patient by id

--- db/migrate/20190313212255_create_admissions.rb
+++ db/migrate/20190313212255_create_admissions.rb
@@ -3,9 +3,7 @@ class CreateAdmissions < ActiveRecord::Migration[5.2]
     create_table :admissions do |t|
       t.datetime :moment
       t.integer :patient_id
-      t.integer :diagnosis_id
-      t.integer :symptom_id
-      t.integer :observation_id
+
       t.timestamps
     end
   end
