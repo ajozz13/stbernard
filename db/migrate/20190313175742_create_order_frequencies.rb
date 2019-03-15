0c8f1e0a066aa8515691863e32bb544a88d094c7
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2019-03-15 12:22:14 -0400

    setup id

--- db/migrate/20190313175742_create_order_frequencies.rb
+++ db/migrate/20190313175742_create_order_frequencies.rb
@@ -3,6 +3,7 @@ class CreateOrderFrequencies < ActiveRecord::Migration[5.2]
     create_table :order_frequencies do |t|
       t.string :value
       t.integer :unit
+      t.integer :medication_order_id
 
       t.timestamps
     end
