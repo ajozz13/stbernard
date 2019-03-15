8808b4f1aa29684fb4d2fd8f41844814adb1013b
Alberto Ochoa <alberto.ochoa@ibcinc.com>
2019-03-15 08:10:10 -0400

    update enum

--- app/models/medication_order.rb
+++ app/models/medication_order.rb
@@ -1,5 +1,5 @@
 class MedicationOrder < ApplicationRecord
-  has_one :mass_unit
-  has_one :medication_route
   has_one :order_frequency
+  enum route: [ :po, :im, :sc ]
+  enum unit: [ :mg, :g ]
 end
